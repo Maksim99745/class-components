395cc3ed139ac02663161004eed9d6366beec2ae
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-07-09 18:14:03 +0300

    fix: chech deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
