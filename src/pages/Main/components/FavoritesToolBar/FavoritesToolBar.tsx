1a0e52028f4499648ed88eddf510a83bd69398f0
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2024-07-18 00:43:25 +0300

    feat: download without libraries

--- src/pages/Main/components/FavoritesToolBar/FavoritesToolBar.tsx
+++ src/pages/Main/components/FavoritesToolBar/FavoritesToolBar.tsx
@@ -1,4 +1,5 @@
-import { CSVLink } from 'react-csv';
+import { Character } from '@models/character';
+import { saveAs } from 'file-saver';
 import { useSelector } from 'react-redux';
 import { RootState } from 'src/store/store';
 import { useMainPageActions } from '../../hooks/useMainPageActions';
@@ -8,7 +9,36 @@ export default function FavoritesToolBar() {
   const favorites = useSelector((state: RootState) => state.favorites);
   const { unselectAll } = useMainPageActions();
 
-  // const downloadCSV = () => generateCSV(favorites, `${favorites.length}_favorites.csv`);
+  const arrayToCSV = (data: Character[]): string => {
+    if (data.length === 0) {
+      return '';
+    }
+    function isKeyOfCharacter(key: string): key is keyof Character {
+      return key in data[0];
+    }
+    const headers: Array<keyof Character> = Object.keys(data[0]).filter(isKeyOfCharacter);
+    const csvRows = [
+      headers.join(','),
+      ...data.map((row) =>
+        headers
+          .map((header) => {
+            const value = row[header];
+            const escaped = `${value}`.replace(/"/g, '\\"');
+            return `"${escaped}"`;
+          })
+          .join(','),
+      ),
+    ];
+
+    return csvRows.join('\n');
+  };
+
+  const handleDownload = () => {
+    const csvData = arrayToCSV(favorites);
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    const filename = `${favorites.length}_favorites.csv`;
+    saveAs(blob, filename);
+  };
 
   return (
     <div className={styles.favoritesToolbar}>
@@ -17,10 +47,8 @@ export default function FavoritesToolBar() {
         <button type="button" onClick={() => unselectAll()}>
           Unselect all
         </button>
-        <button type="button">
-          <CSVLink data={favorites} style={{ color: 'black' }}>
-            Download
-          </CSVLink>
+        <button type="button" onClick={() => handleDownload()}>
+          Download
         </button>
       </div>
     </div>
